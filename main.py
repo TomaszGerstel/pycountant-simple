a27bf395d1c38582797c6707f6232d140168f35b
Tomasz <gerstel.tomasz@gmail.com>
2022-07-12 13:03:20 +0200

    addMethodToBalance

--- main.py
+++ main.py
@@ -44,9 +44,6 @@ class DefaultVatInvoice(Invoice):
         super().__init__(amount, client, worker, descr)
         self.__set_net_amount_and_vat()
 
-    # def __post_init__(self):
-    #     self.__set_net_amount_and_vat()
-
     def __set_net_amount_and_vat(self):
         self.vat_per_cent = 30
         self.net_amount = self.amount / (100 + 30) * 100
@@ -84,9 +81,6 @@ class Transfer:
     _from: Optional[str] = None
     _to: Optional[str] = None
     amount: Optional[float] = None
-    # net_amount: Optional[float] = None
-    # vat_per_cent: Optional[float] = None
-    # vat_value: Optional[float] = None
     date: datetime.datetime = datetime.date.today()
     descr: str = ""
 
@@ -117,23 +111,32 @@ def sum_transfers_generator(tr_arr):
 
 class AmountBalance:
     tr_arr: []
-    gross_balance: [float]
+    costs: [float]
+    gross_income: [float]
+    balance: [float]
     net_balance: [float]
     vat_balance: [float]
 
     def __init__(self, tr_arr):
         self.tr_arr = tr_arr
-        self.gross_balance = self.__gross_balance(self.tr_arr)
+        self.costs = self.__get_costs(self.tr_arr)
+        self.gross_income = self.__gross_income(self.tr_arr)
+        self.balance = self.gross_income - self.costs
         self.net_balance = self.__net_balance(self.tr_arr)
         self.vat_balance = self.__vat_balance(self.tr_arr)
 
-    def __gross_balance(self, tr_arr):
+    def __get_costs(self, tr_arr):
+        _sum = 0
+        for t in tr_arr:
+            if t.transfer_type == TransferType.OUT_TRANSFER:
+                _sum += t.amount
+        return _sum
+
+    def __gross_income(self, tr_arr):
         _sum = 0
         for t in tr_arr:
             if t.transfer_type == TransferType.IN_TRANSFER:
                 _sum += t.amount
-            if t.transfer_type == TransferType.OUT_TRANSFER:
-                _sum -= t.amount
         return _sum
 
     def __net_balance(self, tr_arr):
@@ -174,9 +177,13 @@ def main():
     print()
     balance = AmountBalance(tr_arr)
 
-    print("brutto")
-    print(balance.gross_balance)
-    print("netto")
+    print("stan rachunku")
+    print(balance.balance)
+    print("przychód, podstawa do vat")
+    print(balance.gross_income)
+    print("koszta, wychodzące")
+    print(balance.costs)
+    print("zysk, netto, pomniejszony o koszta i vat")
     print(balance.net_balance)
     print("vat")
     print(balance.vat_balance)
