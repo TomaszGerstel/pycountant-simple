e614e007af14a52ced6be5538c663331de240747
tomasz <gerstel.tomasz@gmail.com>
2022-07-23 12:55:30 +0200

    more tests from stories

--- tests/countant_test.py
+++ tests/countant_test.py
@@ -106,7 +106,6 @@ class TestCountant:
 
 
 class TestExampleStories:
-
     """Unit tests to story 1. from stories.md file"""
 
     """Receipt with set fixed vat = 20%"""
@@ -166,4 +165,77 @@ class TestExampleStories:
         is profit and can be get as salary"""
         assert self.balance2.profit == 315
 
+    """Testing story 3. from stories.md"""
 
+    """ticket/hotel - reimbursement transfer to personal account 500 EUR"""
+    rec3 = Receipt(amount=500, client="me", worker="Ryan Air", descr="ticket")
+    transfer3 = Transfer(transfer_type=TransferType.OUT_TRANSFER, receipt=rec3, amount=500)
+    """list included transfer from story 1, 2 and above reimbursement"""
+    arr3 = [transfer1, transfer2, transfer3]
+    balance3 = BalanceOfFinances(arr3)
+
+    def test_balance_calc_for_example_story_03(self):
+        """now costs are 60+500 for ticket -> 560"""
+        assert self.balance3.costs == 560
+        """and balance is 600-60-500 -> 40"""
+        assert self.balance3.balance == 40
+
+    """Unit tests to story 4. from stories.md file"""
+
+    """Receipt without set vat (default vat percentage is 0)"""
+    rec4 = Receipt(
+        amount=500,
+        worker="me",
+        client="Masterkelm",
+        descr="for service",
+    )
+
+    """Incoming transfer with above receipt"""
+    transfer4 = Transfer(TransferType.IN_TRANSFER, receipt=rec4)
+
+    """passing the transfer object to the calculations object"""
+    arr4 = [transfer4]
+    balance4 = BalanceOfFinances(arr4)
+
+    def test_balance_calc_for_example_story_04(self):
+        """gross income is 500"""
+        assert self.balance4.gross_income == 500
+        """balance is 600 (with expenses = 0)"""
+        assert self.balance4.balance == 500
+        """vat balance: 500 + 0% vat is amount = 500 >> vat = 0"""
+        assert self.balance4.vat_balance == 0
+        """net balance: without vat equals gross income = 500"""
+        assert self.balance4.net_balance == 500
+        """income tax 30% from 500 (amount without vat) is 150.
+        have to pay to tax office"""
+        assert self.balance4.income_tax_30 == 150
+        """profit: 70% from net income (500) = 350
+        and can be transfer to personal bank account"""
+        assert self.balance4.profit == 350
+        """there was no expense, it should return 0"""
+        assert self.balance4.costs == 0
+
+    """Unit tests to story 4 + 2. from stories.md file"""
+
+    """adding transfers from stories 2 and 4"""
+    arr6 = [transfer2, transfer4]
+    balance6 = BalanceOfFinances(arr6)
+
+    def test_balance_calc_for_example_story_06(self):
+        """gross income is 500"""
+        assert self.balance6.gross_income == 500
+        """balance is 600 (with expenses = 60)"""
+        assert self.balance6.balance == 440
+        """vat balance: 0% from 500 - 20% from 60 -> vat = -10
+        you paid 10 EUR more vat than you should to treasury """
+        assert self.balance6.vat_balance == -10
+        """net balance: without vat equals gross income = 500-50"""
+        assert self.balance6.net_balance == 450
+        """income tax 30% from 450 (amount without vat) is 135.
+        have to pay to tax office"""
+        assert self.balance6.income_tax_30 == 135
+        """profit: 70% from net income (450) = 325
+        and can be transfer to personal bank account"""
+        assert self.balance6.profit == 315
+        """there was one expense for 60"""
+        assert self.balance6.costs == 60
