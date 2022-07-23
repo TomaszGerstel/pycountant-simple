888b8f786d586423b839dca4849f67fb841fbf1e
tomasz <gerstel.tomasz@gmail.com>
2022-07-23 11:34:57 +0200

    changing the implementation of methods in calculations

--- pycountant/calculations.py
+++ pycountant/calculations.py
@@ -18,12 +18,12 @@ class BalanceOfFinances:
 
     def __init__(self, tr_arr):
         self.tr_arr = tr_arr
-        self.costs = self.__get_costs(tr_arr)
-        self.gross_income = self.__gross_income(tr_arr)
+        self.costs = get_costs(tr_arr)
+        self.gross_income = get_gross_income(tr_arr)
         self.balance = self.gross_income - self.costs
-        self.net_balance = self.__net_balance(tr_arr)
-        self.vat_balance = self.__vat_balance(tr_arr)
-        self.income_tax_30 = self.__calc_income_tax_30(self.net_balance)
+        self.net_balance = get_net_balance(tr_arr)
+        self.vat_balance = get_vat_balance(tr_arr)
+        self.income_tax_30 = get_calc_income_tax_30(self.net_balance)
         self.profit = self.net_balance - self.income_tax_30
 
     def __repr__(self):
@@ -34,45 +34,8 @@ class BalanceOfFinances:
             f"income tax: {self.income_tax_30}; profit: {self.profit}\n"
         )
 
-    def __get_costs(self, tr_arr):
-        _sum = 0
-        for t in tr_arr:
-            if t.transfer_type == TransferType.OUT_TRANSFER:
-                _sum += t.receipt.amount
-        return _sum
 
-    def __gross_income(self, tr_arr):
-        _sum = 0
-        for t in tr_arr:
-            if t.amount <= 0:
-                raise NegativeValueError("amount can't be negative value")
-            if t.transfer_type == TransferType.IN_TRANSFER:
-                _sum += t.amount
-        return _sum
-
-    def __net_balance(self, tr_arr):
-        _sum = 0
-        for t in tr_arr:
-            if t.transfer_type == TransferType.IN_TRANSFER:
-                _sum += t.receipt.net_amount
-            if t.transfer_type == TransferType.OUT_TRANSFER:
-                _sum -= t.receipt.net_amount
-        return _sum
-
-    def __vat_balance(self, tr_arr):
-        _sum = 0
-        for t in tr_arr:
-            if t.transfer_type == TransferType.IN_TRANSFER:
-                _sum += t.receipt.vat_value
-            if t.transfer_type == TransferType.OUT_TRANSFER:
-                _sum -= t.receipt.vat_value
-        return _sum
-
-    def __calc_income_tax_30(self, income):
-        return income * config.income_tax_pct / 100.0
-
-
-def __get_costs(tr_arr):
+def get_costs(tr_arr):
     _sum = 0
     for t in tr_arr:
         if t.transfer_type == TransferType.OUT_TRANSFER:
@@ -80,7 +43,7 @@ def __get_costs(tr_arr):
     return _sum
 
 
-def __gross_income(tr_arr):
+def get_gross_income(tr_arr):
     _sum = 0
     for t in tr_arr:
         if t.amount <= 0:
@@ -90,7 +53,7 @@ def __gross_income(tr_arr):
     return _sum
 
 
-def __net_balance(tr_arr):
+def get_net_balance(tr_arr):
     _sum = 0
     for t in tr_arr:
         if t.transfer_type == TransferType.IN_TRANSFER:
@@ -100,7 +63,7 @@ def __net_balance(tr_arr):
     return _sum
 
 
-def __vat_balance(tr_arr):
+def get_vat_balance(tr_arr):
     _sum = 0
     for t in tr_arr:
         if t.transfer_type == TransferType.IN_TRANSFER:
@@ -110,7 +73,7 @@ def __vat_balance(tr_arr):
     return _sum
 
 
-def __calc_income_tax_30(income):
+def get_calc_income_tax_30(income):
     return income * config.income_tax_pct / 100.0
 
 
@@ -127,12 +90,12 @@ class BalanceResults:
 
 
 def calculate_balance(tr_arr) -> BalanceResults:
-    costs = __get_costs(tr_arr)
-    gross_income = __gross_income(tr_arr)
+    costs = get_costs(tr_arr)
+    gross_income = get_gross_income(tr_arr)
     balance = gross_income - costs
-    net_balance = __net_balance(tr_arr)
-    vat_balance = __vat_balance(tr_arr)
-    income_tax_30 = __calc_income_tax_30(net_balance)
+    net_balance = get_net_balance(tr_arr)
+    vat_balance = get_vat_balance(tr_arr)
+    income_tax_30 = get_calc_income_tax_30(net_balance)
     profit = net_balance - income_tax_30
     return BalanceResults(
         costs=costs,
