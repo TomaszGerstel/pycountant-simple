8a9a8c7a8e54f4819e751b522be7bb7fc89a7d09
Tomasz <gerstel.tomasz@gmail.com>
2022-11-02 20:26:29 +0100

    adding some test calculations

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -14,6 +14,9 @@ def test_example_transfers_balance(
     transfer_for_receipt2,
     transfer_for_receipt3,
     transfer4,
+    transfer5,
+    transfer6,
+    transfer7
 ):
 
     # Given
@@ -22,8 +25,11 @@ def test_example_transfers_balance(
     tr2 = transfer_for_receipt2
     tr3 = transfer_for_receipt3
     tr4 = transfer4
+    tr5 = transfer5
+    tr6 = transfer6
+    tr7 = transfer7
 
-    tr_arr1 = [tr1, tr2, tr3]
+    tr_arr1 = [tr1, tr2, tr3, tr5, tr6, tr7]
     tr_arr2 = [tr4]
     rec_arr1 = [rec1, rec2, rec3]
     rec_arr2 = [rec4]
@@ -32,8 +38,9 @@ def test_example_transfers_balance(
     balance = calculate_balance(tr_arr1, rec_arr1)
 
     # Then
-    # 650 + 260(in transfers) - 130(out transfer) = 780
-    assert balance.balance == 780.0
+    # 650 + 260(in transfers) - 130(out transfer) - 200(salary)
+    # - 100(vat paid) - 80(tax paid to tax office) = 400
+    assert balance.balance == 400.0
     # 130 out transfer
     assert balance.costs == 130.0
     # 650 + 260 in transfers = 910
@@ -49,6 +56,20 @@ def test_example_transfers_balance(
     assert balance.due_tax_30 == 180.0
     # net balance (600) - income tax to pay (180) equals 420
     assert balance.due_profit == 420.0
+    # paid profit: one salary transfer with 200
+    assert balance.paid_profit == 200.0
+    # remaining profit: due profit - paid profit: 420 - 200 = 220
+    assert balance.remaining_profit == 220.0
+    # paid vat: one transfer with 100
+    assert balance.paid_vat == 100.0
+    # vat balance: due vat - paid vat: 180 - 100 = 80
+    assert balance.vat_balance == 80.0
+    # paid tax: one transfer with 80
+    assert balance.paid_tax == 80.0
+    # income ta 30 balance: due tax - paid tax: 180 - 80 = 100
+    assert balance.income_tax_30 == 100
+    # other costs: paid profit, vat and tax: 200 + 100 + 80 == 380
+    assert balance.other_costs == 380
     # try to make calculations for transaction with negative value
     with pytest.raises(NegativeValueError):
         balance2 = calculate_balance(tr_arr2, rec_arr2)
