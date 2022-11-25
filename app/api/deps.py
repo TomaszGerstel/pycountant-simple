902c930b5de4a479daf4df659005ce1302a2a4ed
Tomasz <gerstel.tomasz@gmail.com>
2022-11-25 16:59:14 +0100

    add requirement

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,13 +1,9 @@
 import os
-from datetime import date
-
-from fastapi import Path
-from fastapi.params import Form
 
 from fastapi_login.fastapi_login import LoginManager
 
-from db import crud_transfer, crud_receipt, crud_user
-from pycountant.calculations import current_balance, balance_to_date_range
+from db import crud_user
+
 
 from db.session import Session
 
@@ -23,26 +19,4 @@ current_user_name = None
 @manager.user_loader
 def load_user(username):
     user = crud_user.get(name=username, session=session)
-    # manager.lump_sum_tax = user.lump_sum_tax_rate
     return user
-
-#
-# def get_transfers():
-#     return crud_transfer.get_all(session, current_user_id, 10)
-#
-#
-# def get_receipts():
-#     return crud_receipt.get_all(session, current_user_id)
-#
-#
-# def get_balance():
-#     # print("TYPE:", current_balance(session))
-#     print("rate", lump_sum_tax_rate)
-#     return current_balance(session, current_user_id, lump_sum_tax_rate)
-#
-#
-# def get_receipts_without_transfer():
-#     return crud_receipt.get_all_without_transfer(session, current_user_id)
-
-# def get_user(name):
-#     return crud_user.get(name, session)
